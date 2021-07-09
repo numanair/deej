c8cf207d43f0f5101958264c05837c8743ff4876
numanair <numanair@gmail.com>
2021-07-09 01:38:18 -0700

    Add MultiMap for deadzone adjustment
    Added MultiMap library to use an array of measured actual potentiometer outputs. This should improve the deadzone at the lower end of the slider.

--- arduino/deej-5-sliders-LGTX8P/deej-5-sliders-LGTX8P.ino
+++ arduino/deej-5-sliders-LGTX8P/deej-5-sliders-LGTX8P.ino
@@ -1,10 +1,20 @@
 #include <avr/power.h>
 #include "lgtx8p.h"
+#include "MultiMap.h"
 
 const int NUM_SLIDERS = 5;
 const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
-const int ADC_bits = 10; // 10 or 12-bit
-const int MaxADC = 1016; // 4064 for 12-bit or 1016 for 10-bit
+const int ADC_bits = 12; // 10 or 12-bit
+
+//float input[] =  {0,  8,  33,  99, 219, 350, 925, 994, 1016}; //10-bit ADC, not as good :)
+
+// Measured values ever 5mm of throw
+//float input[] =  {0, 32,    145,   353,    909, 1493,   2016,  2549,  3106, 3679,   3960, 4064};
+//float output[] = {0, 85.25, 170.5, 255.75, 341, 426.25, 511.5, 596.75, 682, 767.25, 852.5, 1023};
+
+// Adjusted multiMap for compromise on precision and linearity
+float input[] =  {0, 32,    145,   353,    909,  4064};
+float output[] = {0, 85.25, 170.5, 255.75, 341,  1023};
 
 int analogSliderValues[NUM_SLIDERS];
 
@@ -27,7 +37,8 @@ void loop() {
 
 void updateSliderValues() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
-      analogSliderValues[i] = map(analogRead(analogInputs[i]), 0, MaxADC, 0, 1023);
+      analogSliderValues[i] = multiMap<float>(analogRead(analogInputs[i]), input, output, 6);
+      //analogSliderValues[i] = analogRead(analogInputs[i]); //unfiltered output
   }
 }
 
