e00f77cd924321e06846026bfe9962e8c28d9dbb
numanair <numanair@gmail.com>
2022-02-23 14:07:41 -0800

    remove wait for serial as it blocks MIDI output

--- arduino/deej-MIDI-stm32/deej-MIDI-stm32.ino
+++ arduino/deej-MIDI-stm32/deej-MIDI-stm32.ino
@@ -4,7 +4,7 @@
 #include <BlockNot.h>
 
 // Number of potentiometers or faders
-const int NUM_SLIDERS = 5;
+const uint8 NUM_SLIDERS = 5;
 
 // Potentiometer pins assignment
 const uint8 analogInputs[NUM_SLIDERS] = {0, 1, 2, 3, 4};
@@ -12,29 +12,27 @@ const uint8 analogInputs[NUM_SLIDERS] = {0, 1, 2, 3, 4};
 // Adjusts linearity correction for my specific potentiometers.
 // 1 = fully linear but jittery. 0.7 is about max for no jitter.
 const float correctionMultiplier = 0.70;
-
-// measured output every equal 5mm increment in 14-bit. Minimum and maximum values are not affected by correctionMultiplier.
-float measuredInput[] = {14, 50, 165,  413, 907, 1450, 1975, 2545, 3095, 3645, 3923, 4030, 4088};
-
-// const uint8 pot_pin = 6;
 const uint8 threshold = 1;
 
-const unsigned int midi_channel[NUM_SLIDERS] = {1, 2, 3, 4, 5};
-const unsigned int cc_command[NUM_SLIDERS] = {7, 7, 7, 7, 7}; // MIDI CC number
+// measured output every equal 5mm increment in 12-bit. Minimum and maximum values are not affected by correctionMultiplier.
+const float measuredInput[] = {14, 50, 165,  413, 907, 1450, 1975, 2545, 3095, 3645, 3923, 4030, 4088};
 
-// number of elements in the MultiMap arrays
+const uint8 midi_channel[NUM_SLIDERS] = {1, 2, 3, 4, 5};
+const uint8 cc_command[NUM_SLIDERS] = {7, 7, 7, 7, 7}; // MIDI CC number
+
+// Calculate number of elements in the MultiMap arrays
 const int arrayQty = sizeof(measuredInput) / sizeof(measuredInput[0]);
 float adjustedinputval[arrayQty] = {0};
 
 BlockNot timer(10); // milliseconds between sending serial data
 
 // Probably no need to change these calculated values
-float idealOutputValues[arrayQty] = {0, 341, 682, 1024, 1365, 1706, 2048, 2389, 2730, 3072, 3413, 3754, 4096};
-// Note: 16383 = 2¹⁴ - 1 (the maximum value that can be represented by
-// a 14-bit unsigned number
+float idealOutputValues[arrayQty] = {0, 341, 682, 1024, 1365, 1706, 2048, 2389, 2730, 3072, 3413, 3754, 4095};
+// Note: 4095 = 2^12 - 1 (the maximum value that can be represented by
+// a 12-bit unsigned number
 
-unsigned int old_value[NUM_SLIDERS] = {0};
-unsigned int new_value[NUM_SLIDERS] = {0};
+uint8 old_value[NUM_SLIDERS] = {0};
+uint8 new_value[NUM_SLIDERS] = {0};
 float analogSliderValues[NUM_SLIDERS];
 
 USBMIDI midi;
@@ -54,22 +52,35 @@ void setup() {
   CompositeSerial.registerComponent();
   midi.registerComponent();
   USBComposite.begin();
-  USBComposite.setProductId(0x0031);
-  USBComposite.setProductString("MIX5R");
+//  USBComposite.setProductId(0x0031);
+//  USBComposite.setProductId(0x0483);
+//  USBComposite.setProductString("MIX5R");
+
+  USBComposite.setVendorId(0x0483); // STMicroelectronics
+  USBComposite.setProductId(0xf7cc);
+  USBComposite.setManufacturerString("STMicroelectronics");
+  USBComposite.setProductString("STM32MIDI-MIX5R");
+
+//  USBComposite.setVendorId(uint16 vendor);
+//  USBComposite.setProductId(uint16 product);
+//  USBComposite.setManufacturerString(const char* manufacturer);
+//  USBComposite.setProductString(const char* product);
+//  USBComposite.setSerialString(const char* serialNumber);
 
   // multiplier correction
   for (size_t i = 0; i < arrayQty; i++) {
     adjustedinputval[i] = round(idealOutputValues[i] + (measuredInput[i] - idealOutputValues[i]) * correctionMultiplier);
     // theoretical ideal + (measured - theoretical) * multi
   }
+  // Excludes min and max from adjustment by correctionMultiplier
   adjustedinputval[0] = measuredInput[0]; // min value
   adjustedinputval[arrayQty] = measuredInput[arrayQty]; // max value
 
   midi.begin();
   CompositeSerial.begin(9600);
 
-  while(!CompositeSerial.isConnected()); // wait for usb serial
-  delay(500);
+  delay(1000);
+//  while(!CompositeSerial.isConnected()); // wait for usb serial
 }
 
 void loop() {
